eac4369344bee402fd7e7f9e906cb0f26a0c4555
alex241262 <alexandreferreira168@gmail.com>
2021-08-31 21:28:59 -0400

    finalizado

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("snake");//cria elemento que vai rodar o jogo
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = [];
+let snake = [];//cria cobrinha como lista,ja que ela vai ser um serie de cordenadas,que quando pintadas pintam quadrinhos
 snake[0] = {
     x: 8 * box,
     y: 8 * box
@@ -9,16 +9,16 @@ snake[0] = {
 let direction ="right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 +1) * box
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);//desenha o retangulo usando x e y e a largura e altura setadas.
 }
 
 function criarCobrinha(){
-    for(i=0;i< snake.length;i++){
+    for(i = 0;i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -27,21 +27,29 @@ function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box ,box);
 }
-
+//qunado um evento acontece,detecta e chama afunçao
 document.addEventListener('keydown', update);
 
 function update(event){
-    if(event.keyCode ==37 && direction != "right") direction = "left";
+    if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0   && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0   && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1;i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snnake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert(':( que pena :( !!!');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -55,12 +63,18 @@ function iniciarJogo(){
     if(direction == "up")snakeY -= box;
     if(direction == "down")snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != snake.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1)* box;
+        food.y = Math.floor(Math.random() * 15 + 1)* box;
+    }
 
     let newHead = {
         x: snakeX,
         y: snakeY
     }
-    snake.unshift(newHead);
+    snake.unshift(newHead);//adiciona como primeiro quadrino da cobrinha
 }
 let jogo= setInterval(iniciarJogo, 100);
